35aee06cf6bd2531b372223443f97ca56ad53e26
yuzhicheng <yuzhicheng@innobuddy.com>
2018-10-25 12:01:41 +0800

    add redis subcommand

--- src/commands/zhike/redis.js
+++ src/commands/zhike/redis.js
@@ -4,8 +4,9 @@ const _ = require('lodash')
 const Consul = require('zhike-consul')
 const consulConfig = require('../../../consul.json')
 const Utils = require('../../../../zignis/src/common/utils')
+const Redis = require('ioredis')
 
-exports.command = 'redis cmd key [...arguments]'
+exports.command = 'redis cmd [arguments..]'
 exports.desc = 'zhike redis review, cmd could be get, set, hget, hset and so on.'
 
 exports.builder = function(yargs) {
@@ -16,10 +17,6 @@ exports.handler = function(argv) {
   co(function*() {
     const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development' // development/production/test
 
-    // const keysPrefix = []
-    // argv.keys.map((key) => {
-    //   keysPrefix.push(key.split('.')[0])
-    // })
     const consul = new Consul(
       ['redis'],
       consulConfig[env].host,
@@ -32,56 +29,16 @@ exports.handler = function(argv) {
     )
     const data = yield consul.pull(env)
     const config = data.CFG
-    const cache = require('redis').createClient(
-      config.redis.port,
-      config.redis.host,
-      { password: config.redis.password ? config.redis.password : undefined }
-    )
+    const cache = new Redis(config.redis)
+    const ret = yield cache[argv.cmd].apply(cache, argv.arguments)
 
-    console.log('here')
+    if (argv.json) {
+      Utils.log(JSON.parse(ret))
+    } else {
+      Utils.log(ret)
 
-    switch (argv.cmd) {
-      case 'get':
-        const ret = yield thunkify(cache.get.bind(cache))(argv.key);
-        console.log(ret)
-        break;
-      case 'set':
-        if (env === 'production') {
-          console.warn('Write options can not be done on production')
-          return;
-        }
-        yield thunkify(cache.setex.bind(cache))(argv.key, argv.expires, argv.value);
-        break;
-      case 'mget':
-        yield thunkify(cache.mget.bind(cache))(key)
-        break;
-      case 'hget':
-
-        break;
-      case 'hset':
-        if (env === 'production') {
-          console.warn('Write options can not be done on production')
-          return;
-        }
-        break;
-      case 'hmset':
-
-        break;
-      case 'hgetall':
-
-        break;
-      case 'expire':
-
-        break;
     }
 
     process.exit(0)
-
-    // if (argv.quiet) {
-    //   console.log(JSON.stringify(pickNeededFromPull, null, 2));
-    // }
-    // else {
-    //   Utils.log(pickNeededFromPull)
-    // }
   })
 }
