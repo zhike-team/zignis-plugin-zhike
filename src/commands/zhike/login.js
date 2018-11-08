9528433a42d8f50a380c85141f2d87f87387adf1
yuzhicheng <yuzhicheng@innobuddy.com>
2018-11-08 20:21:48 +0800

    use chalk colorful log

--- src/commands/zhike/login.js
+++ src/commands/zhike/login.js
@@ -1,6 +1,7 @@
 const co = require('co')
 const path = require('path')
 
+const { Utils } = require('zignis')
 const DatabaseLoader = require('../../common/db')
 const db = new DatabaseLoader({ loadReturnInstance: true })
 
@@ -37,7 +38,8 @@ exports.handler = function (argv) {
       name: user['Profile.nickname']
     }
 
-    console.log('先打开智课网，然后打开开发者工具，最后将下面的 JS 代码 copy 到浏览器 Console 中执行和刷新即可登录。')
+    const text = Utils.chalk.underline.bold.cyan('使用方法：先打开智课网，然后打开开发者工具，最后将下面的 JS 代码 copy 到浏览器 Console 中执行和刷新即可登录。')
+    console.log(text)
     console.log('')
     console.log(`document.cookie='ss_user=${encodeURIComponent(JSON.stringify(ssUserCookie))};domain=.smartstudy.com;path=/;'`)
     console.log('')
