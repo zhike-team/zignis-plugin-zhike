c1076995233f2999a4d293488cb8437319609101
yuzhicheng <yuzhicheng@innobuddy.com>
2018-11-09 19:06:28 +0800

    login support phonenumber

--- src/commands/zhike/login.js
+++ src/commands/zhike/login.js
@@ -1,6 +1,8 @@
 const co = require('co')
 const path = require('path')
-
+const inquirer = require('inquirer')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 const { Utils } = require('zignis')
 const DatabaseLoader = require('../../common/db')
 const db = new DatabaseLoader({ loadReturnInstance: true })
@@ -11,11 +13,26 @@ exports.desc = 'way to login zhike'
 exports.builder = function (yargs) {
 }
 
+const processSelectedUser = function (user) {
+  const ssUserCookie = {
+    id: user.id,
+    token: user.tokenPc || user.tokenMobile,
+    name: user['Profile.nickname']
+  }
+
+  const text = Utils.chalk.underline.bold.cyan('使用方法：先打开智课网，然后打开开发者工具，最后将下面的 JS 代码 copy 到浏览器 Console 中执行和刷新即可登录。')
+  console.log(text)
+  console.log('')
+  console.log(`document.cookie='ss_user=${encodeURIComponent(JSON.stringify(ssUserCookie))};domain=.smartstudy.com;path=/;'`)
+  console.log('')
+  process.exit(0)
+}
+
 exports.handler = function (argv) {
   co(function* () {
     const userDatabase = yield db.load('db.user', 'user', db.associate(path.resolve(__dirname, '../../models/user')))
     const { Account, Profile } = userDatabase.models
-    const user = yield Account.findOne({
+    const users = yield Account.findAll({
       raw: true,
       include: [{
         model: Profile,
@@ -23,28 +40,49 @@ exports.handler = function (argv) {
         required: true
       }],
       where: {
-        id: argv.userId
-      }
+        [Op.or]: [
+          { id: argv.userId },
+          { phone: argv.userId },
+          { cellphone: `${argv.userId}` }
+        ],
+        groupId: 1,
+        status: 1
+      },
+      limit: 10
     })
 
-    if (!user) {
+    if (!users) {
       console.log('User not found');
       return;
     }
 
-    const ssUserCookie = {
-      id: user.id,
-      token: user.tokenPc || user.tokenMobile,
-      name: user['Profile.nickname']
+    if (users.length === 1) {
+      processSelectedUser(users[0])
+    } else {
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'selected',
+            message: `Please choose a user to continue:`,
+            choices: users.map(u => {
+              return { name: `(id: ${u.id}) - ${u.phone}`, value: u }
+            }),
+            validate: function(answers) {
+              if (answers.length < 1) {
+                return 'Please choose at least one.'
+              }
+              return true
+            }
+          }
+        ])
+        .then(function(answers) {
+          processSelectedUser(answers.selected)
+        })
+        .catch(function(e) {
+          console.log(e.stack)
+        })
     }
-
-    const text = Utils.chalk.underline.bold.cyan('使用方法：先打开智课网，然后打开开发者工具，最后将下面的 JS 代码 copy 到浏览器 Console 中执行和刷新即可登录。')
-    console.log(text)
-    console.log('')
-    console.log(`document.cookie='ss_user=${encodeURIComponent(JSON.stringify(ssUserCookie))};domain=.smartstudy.com;path=/;'`)
-    console.log('')
-    
-    process.exit(0)
 
   })
 }
