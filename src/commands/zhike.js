104fa8e662fc3f86773b7173959246da08f67a59
yuzhicheng <yuzhicheng@innobuddy.com>
2018-11-30 18:20:22 +0800

    adjust subcommand extend

--- src/commands/zhike.js
+++ src/commands/zhike.js
@@ -1,8 +1,11 @@
+const { Utils } = require('zignis')
+
 exports.command = 'zhike'
 exports.desc = 'Zhike related commands'
 
 exports.builder = function(yargs) {
-  yargs.commandDir('zhike')
+  // yargs.commandDir('zhike')
+  Utils.extendSubCommand('zhike', 'zignis-plugin-zhike', yargs, __dirname)
 }
 
 exports.handler = function(argv) {}
