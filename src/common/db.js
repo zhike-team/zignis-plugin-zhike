cf0a2216d86ae7b57247eff4cd63cecc2b1a9e76
yuzhicheng <yuzhicheng@innobuddy.com>
2018-12-05 15:21:49 +0800

    fix readonly option issue

--- src/common/db.js
+++ src/common/db.js
@@ -79,7 +79,7 @@ class DatabaseLoader {
       sequelize.dropAllSchemas = forbiddenMethod // 删除所有的 postgres schema，即删掉整个数据库
       sequelize.dropSchema = forbiddenMethod // 删除一个 postgres schema，一般也相当于删掉整个数据库
 
-      if (that.options.readonly && process.env.NODE_ENV === 'production') {
+      if (that.options.readonly || process.env.NODE_ENV === 'production') {
         sequelize.query = forbiddenMethod
       }
 
@@ -135,7 +135,7 @@ class DatabaseLoader {
           model.drop = forbiddenMethod // 以防误删表
           model.sync = forbiddenMethod
 
-          if (that.options.readonly && process.env.NODE_ENV === 'production') {
+          if (that.options.readonly || process.env.NODE_ENV === 'production') {
             model.upsert = forbiddenMethod
             model.truncate = forbiddenMethod
             model.destroy = forbiddenMethod
